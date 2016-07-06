9bf8e1ccfc6daf04a979053b4f58f6f6e7f951f0
FireBuddy <aijun@live.ca>
2016-07-06 11:06:26 -0400

    Update Program.cs

--- Zilean/Program.cs
+++ Zilean/Program.cs
@@ -135,7 +135,7 @@ namespace Zilean
                 {
                    Chat.Print("Receiving damage:"+args.SData.Name);
                     E.Cast(Player.Instance);
-                    Chat.Print("Receiving damage:"+args.SData.Width);
+                    Chat.Print("Receiving damage:"+args.SData.CastDelay);
 
                 }
                 if (args.End.Distance(Player.Instance.Position) >= 100)
