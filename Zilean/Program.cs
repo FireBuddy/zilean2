27263b975dcdcec92dfb22b6befb0026942fc797
FireBuddy <aijun@live.ca>
2016-07-07 09:55:58 -0400

    Update Program.cs

--- Zilean/Program.cs
+++ Zilean/Program.cs
@@ -85,7 +85,7 @@ namespace Zilean
                     {
                         miscMenu.Add(spell.SData.Name,
                             new CheckBox(enemy.ChampionName + " - Q - " + spell.Name, false));
-                        miscMenu.Add((spell.SData.Name + "End"),
+                        miscMenu.Add(spell.SData.Name + "End",
                             new CheckBox(enemy.ChampionName + " - Q - " + spell.Name, false));
                     }
                     else if (spell.Slot == SpellSlot.W)
